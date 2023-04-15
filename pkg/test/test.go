36be5206a0284cf0a3760309c3c0e6e8bac5f08f
Mike Helmick <mike.helmick@gmail.com>
2023-04-15 16:24:55 -0700

    github setup

---
+++ pkg/test/test.go
@@ -0,0 +1,6 @@
+// Package test is just to test CI workflows
+package test
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ pkg/test/test_test.go
@@ -0,0 +1,16 @@
+package test_test
+
+import (
+	"testing"
+
+	"github.com/mikehelmick/go-bananas/pkg/test"
+)
+
+func TestAdd(t *testing.T) {
+
+	c := test.Add(1, 2)
+	if c != 3 {
+		t.Fatalf("got the wrong answer...")
+	}
+
+}
